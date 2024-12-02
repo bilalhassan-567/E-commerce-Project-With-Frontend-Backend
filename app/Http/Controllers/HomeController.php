62a9e254c8ea05da6a08780590be60b876a5a633
bilalhassan-567 <mbilalhassan567@gmail.com>
2024-12-02 17:29:08 +0500

    Front-End added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('Admin.index');
     }
+
+    public function home()
+    {
+        return view('home.index');
+    }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+ @include('home.css')
 </head>
+
 <body>
-    <h1>Hello</h1>
+  <div class="hero_area">
+    <!-- header section strats -->
+   @include('home.header')
+    <!-- end header section -->
+    <!-- slider section -->
+
+    @include('home.slider')
+
+
+    <!-- end slider section -->
+  </div>
+  <!-- end hero area -->
+
+  <!-- shop section -->
+
+ @include('home.shop')
+  <!-- end shop section -->
+
+
+
+
+
+
+
+  <!-- contact section -->
+@include('home.contact')
+
+  
+  <!-- end contact section -->
+@include('home.info')
+   
+
+  <!-- info section -->
+
+  
+
 </body>
+
 </html>
